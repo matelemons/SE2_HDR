dde546d4fb018e081bbd7d3090941d488da4391a
matelemons <matusson.6105@gmail.com>
2026-08-09 11:56:40 +0200

    Fix version issue

---
+++ SE2HDR/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+// Define assembly version when compiled by Pulsar
+#if !DEV_BUILD
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+#endif
